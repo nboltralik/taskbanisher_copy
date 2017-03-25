a32a5e9eee9a1e808ea66de1e4b9c076dc5b0850
David McCoy <damccoy1@crimson.ua.edu>
2017-03-25 14:16:27 -0500

    added stuff from Flask Mega-Tutorial

---
+++ app/views.py
@@ -0,0 +1,35 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+from pytodoist import todoist
+
+@app.route('/')
+@app.route('/index')
+def index():
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    user = todoist.login('davidmccoy@outlook.com', '1001052!')
+    projects = user.get_projects()
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts,
+                           projects=projects)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
