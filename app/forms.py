c7af542f6702a10a5a78b67e8c72c27c54aa93a6
David McCoy <damccoy1@crimson.ua.edu>
2017-03-26 01:07:47 -0500

    looking okay

--- app/forms.py
+++ app/forms.py
@@ -9,8 +9,8 @@ class LoginForm(Form):
 
 
 class CustomizationForm(Form):
-	head = SelectField('Head', choices = [('happy.png', 'Happy Face'), ('sad.png', 'Sad Face'), 'surprised.png', ('Surprised Face')])
-	torso = SelectField('Body', choices = [('blackbody.png', 'Black'), ('greenbody.png', 'Green'), ('bluebody.png', 'Blue')])
-	shoes = SelectField('Shoes', choices = [('blackshoes.png', 'Black'), ('greenshoes.png', 'Green'), ('blueshoes.png', 'Blue')])
+	head = SelectField('Head: ', choices = [('happy.png', 'Happy Face'), ('sad.png', 'Sad Face'), ('surprised.png', 'Surprised Face')])
+	torso = SelectField('Torso: ', choices = [('blackbody.png', 'Black'), ('greenbody.png', 'Green'), ('bluebody.png', 'Blue')])
+	shoes = SelectField('Kicks: ', choices = [('blackshoes.png', 'Black'), ('greenshoes.png', 'Green'), ('blueshoes.png', 'Blue')])
 	titleUpdate = StringField('Update Title')
 	submit = SubmitField('Save Hero!!')
